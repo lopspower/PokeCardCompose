08e0ac0b13bf0149c95f1e60187fd96d93ea1fb1
mikhael.lopez <lopez.mikhael@gmail.com>
2022-08-17 11:18:25 +0200

    [FIX] Presentation Unit Test - Mock Android Logger

---
+++ presentation/src/test/java/android/util/Log.java
@@ -0,0 +1,30 @@
+package android.util;
+
+public class Log {
+    public static int v(String tag, String msg) {
+        System.out.println("VERBOSE: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int d(String tag, String msg) {
+        System.out.println("DEBUG: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int i(String tag, String msg) {
+        System.out.println("INFO: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int w(String tag, String msg) {
+        System.out.println("WARN: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int e(String tag, String msg) {
+        System.out.println("ERROR: " + tag + ": " + msg);
+        return 0;
+    }
+
+    // add other methods if required...
+}
